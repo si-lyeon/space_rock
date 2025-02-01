1464bd1b922b9daf0f398b774fa81ec87148cd34
si-lyeon <166217929+si-lyeon@users.noreply.github.com>
2025-02-01 23:50:53 +0900

    Enemy shoot&explode update
    but start button UI bug raise, dubble exist

--- Script/main.gd
+++ Script/main.gd
@@ -1,21 +1,22 @@
 extends Node2D
 
 @export var rock_scene : PackedScene
+@export var enemy_scene : PackedScene
 
 var screensize = Vector2.ZERO
-
+var level = 0
+var score = 0
+var playing = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screensize = get_viewport().get_visible_rect().size
-	for i in 3:
-		spawn_rock(3)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	if not playing:
+		return
+	if get_tree().get_nodes_in_group("rocks").size() == 0:
+		new_level()
 
 func spawn_rock(size, pos=null, vel=null) -> void:
 	if pos == null:
@@ -30,6 +31,7 @@ func spawn_rock(size, pos=null, vel=null) -> void:
 	r.exploded.connect(self._on_rock_exploded)
 
 func _on_rock_exploded(size, radius, pos, vel) -> void:
+	$ExplosionSound.play()
 	if size <= 1:
 		return
 	for offset in [-1, 1]:
@@ -37,3 +39,50 @@ func _on_rock_exploded(size, radius, pos, vel) -> void:
 		var newpos = pos + dir * radius
 		var newvel = dir * vel.length() * 1.1
 		spawn_rock(size - 1, newpos, newvel)
+	score += 10 * size
+	$HUD.update_score(score)
+
+func new_game() -> void:
+	$Music.play()
+	get_tree().call_group("rocks", "queue_free")
+	level = 0
+	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
+	$Player.reset()
+	await $HUD/Timer.timeout
+	playing = true
+
+func new_level() -> void:
+	$LevelupSound.play()
+	level += 1
+	$HUD.show_message("Wave %s" % level)
+	for i in level:
+		spawn_rock(3)
+	$EnemyTimer.start(randf_range(5, 10))
+
+func game_over() -> void:
+	$Music.stop()
+	playing = false
+	$HUD.game_over()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		if not playing:
+			return
+		get_tree().paused = not get_tree().paused
+		var message = $HUD/VBoxContainer/Message
+		if get_tree().paused:
+			message.text = "Paused"
+			message.show()
+		else:
+			message.text = ""
+			message.hide()
+
+
+func _on_enemy_timer_timeout() -> void:
+	var e = enemy_scene.instantiate()
+	get_tree().root.add_child(e) 
+	# 이거 타이머의 자식일 때, 왜 pause 안 될까? instantiate가 inherit 속성으로 안 되나?
+	e.target = $Player
+	$EnemyTimer.start(randf_range(20, 40))
--- Script/player.gd
+++ Script/player.gd
@@ -6,6 +6,16 @@ extends RigidBody2D
 @export var bullet_scene : PackedScene
 @export var fire_rate = 0.25
 
+@export var max_shield = 100.0
+@export var shield_regen = 5.0
+
+signal lives_changed
+signal dead
+signal shield_changed
+
+var reset_pos = false
+var lives = 0: set = set_lives
+
 var can_shoot = true
 
 var thrust = Vector2.ZERO
@@ -13,6 +23,8 @@ var rotation_dir = 0
 
 enum {INIT, ALIVE, INVULNERABLE, DEAD}
 var state = INIT
+
+var shield = 0: set = set_shield
 
 var screensize = Vector2.ZERO
 
@@ -25,6 +37,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	get_input()
+	shield += shield_regen * delta
 
 func _physics_process(delta: float) -> void:
 	constant_force = thrust
@@ -35,18 +48,30 @@ func _integrate_forces(physics_state: PhysicsDirectBodyState2D) -> void:
 	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
 	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
 	physics_state.transform = xform
+	
+	if reset_pos:
+		physics_state.transform.origin = screensize/2
+		reset_pos = false
 
 # ----------------------------------------------------------------------------
 func change_state(new_state : int) -> void:
 	match new_state:
 		INIT:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.modulate.a = 0.5
 		ALIVE:
 			$CollisionShape2D.set_deferred("disabled", false)
+			$Sprite2D.modulate.a = 1.0
 		INVULNERABLE:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.modulate.a = 0.5
+			$Explosion/InvulnerabilityTimer.start()
 		DEAD:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.hide()
+			$EngineSound.stop()
+			linear_velocity = Vector2.ZERO
+			dead.emit()
 	state = new_state
 
 func get_input() -> void:
@@ -55,6 +80,10 @@ func get_input() -> void:
 		return
 	if Input.is_action_pressed("thrust"):
 		thrust = transform.x * engine_power
+		if not $EngineSound.playing:
+			$EngineSound.play()
+	else:
+		$EngineSound.stop()
 	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
 	if Input.is_action_pressed("shoot") and can_shoot:
 		shoot()
@@ -67,7 +96,48 @@ func shoot() -> void:
 	var b = bullet_scene.instantiate()
 	get_tree().root.add_child(b)
 	b.start($Muzzle.global_transform)
+	$LaserSound.play()
 
 
 func _on_gun_cooldown_timeout() -> void:
 	can_shoot = true
+
+func set_lives(value : int) -> void:
+	lives = value
+	lives_changed.emit(lives)
+	if lives <= 0:
+		change_state(DEAD)
+	else:
+		change_state(INVULNERABLE)
+		shield = max_shield
+
+
+func reset() -> void:
+	reset_pos = true
+	$Sprite2D.show()
+	lives = 3
+	change_state(ALIVE)
+
+
+func _on_invulnerability_timer_timeout() -> void:
+	change_state(ALIVE)
+
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("rocks"):
+		shield -= body.size * 25
+		body.explode()
+
+func explode() -> void:
+	$Explosion.show()
+	$Explosion/AnimationPlayer.play("explosion")
+	await $Explosion/AnimationPlayer.animation_finished
+	$Explosion.hide()
+
+func set_shield(value : int) -> void:
+	value = min(value, max_shield)
+	shield = value
+	shield_changed.emit(shield / max_shield)
+	if shield <= 0:
+		lives -= 1
+		explode()
